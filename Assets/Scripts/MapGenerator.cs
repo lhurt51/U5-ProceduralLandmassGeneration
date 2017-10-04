a9b7631e7a64fba237204eac5c7a24707311d058
Liam Hurt <liamhurt51@gmail.com>
2017-10-04 16:53:08 -0700

    added a mesh render to my scene there is two problems tho the mesh cant be that bit and i need to scale the height

--- Assets/Scripts/MapGenerator.cs
+++ Assets/Scripts/MapGenerator.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class MapGenerator : MonoBehaviour {
 
-    public enum DrawMode { NoiseMap, ColorMap };
+    public enum DrawMode { NoiseMap, ColorMap, Mesh };
     public DrawMode drawMode;
 
     public int mapWidth;
@@ -40,10 +40,12 @@ public class MapGenerator : MonoBehaviour {
         }
 
         MapDisplay display = FindObjectOfType<MapDisplay>();
-        if (drawMode == DrawMode.NoiseMap)
-            display.DrawTexture(TextureGenerator.TextureFromHeightMap(noiseMap));
-        else if (drawMode == DrawMode.ColorMap)
-            display.DrawTexture(TextureGenerator.TextureFromColorMap(colorMap, mapWidth, mapHeight));
+		if (drawMode == DrawMode.NoiseMap)
+			display.DrawTexture(TextureGenerator.TextureFromHeightMap(noiseMap));
+		else if (drawMode == DrawMode.ColorMap)
+			display.DrawTexture(TextureGenerator.TextureFromColorMap(colorMap, mapWidth, mapHeight));
+		else if (drawMode == DrawMode.Mesh)
+			display.DrawMesh(MeshGenerator.GenerateTerrainMesh(noiseMap), TextureGenerator.TextureFromColorMap(colorMap, mapWidth, mapHeight));
 
     }
 
