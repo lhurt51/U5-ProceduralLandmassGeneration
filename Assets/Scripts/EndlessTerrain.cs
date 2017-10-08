52025f21f495bc78a0df34842a5a44df05d31502
Liam Hurt <liamhurt51@gmail.com>
2017-10-08 16:48:07 -0700

    Finnaly on infinate terrain... I already handled LOD generation

---
+++ Assets/Scripts/EndlessTerrain.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EndlessTerrain : MonoBehaviour {
+
+	public const float maxViewDst = 300;
+	public Transform viewer;
+
+	public static Vector2 viewerPosition;
+	int chunkSize;
+	int chunksVisibleInViewDst;
+
+	void Start() {
+		chunkSize = MapGenerator.mapChunkSize - 1;
+		chunksVisibleInViewDst = Mathf.RoundToInt(maxViewDst / chunkSize);
+	}
+
+	void UpdateVisibleChunks() {
+		int currentChunkCoordX = Mathf.RoundToInt(viewerPosition.x / chunkSize);
+		int currentChunkCoordY = Mathf.RoundToInt(viewerPosition.y / chunkSize);
+
+		for (int yOffset = -chunksVisibleInViewDst; yOffset <= chunksVisibleInViewDst; yOffset++) {
+			for (int xOffset = -chunksVisibleInViewDst; xOffset <= chunksVisibleInViewDst; xOffset++) {
+				Vector2 viewedChunkCoord = new Vector2(currentChunkCoordX + xOffset, currentChunkCoordY + yOffset);
+			}
+		}
+	}
+
+}
