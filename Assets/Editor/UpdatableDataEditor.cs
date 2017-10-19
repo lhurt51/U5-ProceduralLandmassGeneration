24504e1ac324a58db37b7829d7438c8a8e23d69d
Liam Daniel Hurt <liamhurt51@gmail.com>
2017-10-19 14:16:41 -0700

    Creating a terrain shadder for my procedural terrain going to add textures next

--- Assets/Editor/UpdatableDataEditor.cs
+++ Assets/Editor/UpdatableDataEditor.cs
@@ -6,12 +6,13 @@ using UnityEditor;
 [CustomEditor(typeof(UpdatableData), true)]
 public class UpdatableDataEditor : Editor {
 
-	public override void OnInspectorGUI ()
-	{
+	public override void OnInspectorGUI () {
 		base.OnInspectorGUI ();
 
 		UpdatableData data = (UpdatableData)target;
-		if (GUILayout.Button ("Update"))
-			data.NotifyOfUpdatedValues();
+        if (GUILayout.Button("Update")) {
+            data.NotifyOfUpdatedValues();
+            EditorUtility.SetDirty(target);
+        }
 	}
 }
--- Assets/Scripts/MapGenerator.cs
+++ Assets/Scripts/MapGenerator.cs
@@ -117,6 +117,8 @@ public class MapGenerator : MonoBehaviour {
 			}
 		}
 
+        textureData.UpdateMeshHeights(terrainMaterial, terrainData.minHeight, terrainData.maxHeight);
+
         return new MapData(noiseMap);
     }
 
