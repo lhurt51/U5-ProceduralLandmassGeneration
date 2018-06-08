b992c1550edd003b5a5afc895a52f58f2dd97979
Liam Hurt <liamhurt51@gmail.com>
2018-06-07 19:01:36 -0700

    Adding a gradient editor for the textures

---
+++ Assets/Editor/GradientDrawer.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+[CustomPropertyDrawer(typeof(CustomGradient))]
+public class GradientDrawer : PropertyDrawer {
+
+	public override void OnGUI(Rect pos, SerializedProperty prop, GUIContent label)
+    {
+        Event guiEvent = Event.current;
+        CustomGradient gradient = (CustomGradient)fieldInfo.GetValue(prop.serializedObject.targetObject);
+        float labelWidth = GUI.skin.label.CalcSize(label).x + 5;
+        Rect textRect = new Rect(pos.x + labelWidth, pos.y, pos.width - labelWidth, pos.height);
+
+        if (guiEvent.type == EventType.Repaint)
+        {
+            GUIStyle gradStyle = new GUIStyle();
+
+            GUI.Label(pos, label);
+            gradStyle.normal.background = gradient.GetTexture((int)pos.width);
+            GUI.Label(textRect, GUIContent.none, gradStyle);
+        }
+        else if (guiEvent.type == EventType.MouseDown && guiEvent.button == 0 && textRect.Contains(guiEvent.mousePosition))
+        {
+            EditorWindow.GetWindow<GradientEditor>();
+        }
+    }
+
+}
--- Assets/Scripts/Terrain.shader
+++ Assets/Scripts/Terrain.shader
@@ -1,4 +1,6 @@
-﻿Shader "Custom/Terrain" {
+﻿// Upgrade NOTE: upgraded instancing buffer 'Props' to new syntax.
+
+Shader "Custom/Terrain" {
 	Properties{
 		testTexture("Texture", 2D) = "white"{}
 		testScale("Scale", Float) = 1
@@ -54,9 +56,9 @@
 		// Add instancing support for this shader. You need to check 'Enable Instancing' on materials that use the shader.
 		// See https://docs.unity3d.com/Manual/GPUInstancing.html for more information about instancing.
 		// #pragma instancing_options assumeuniformscaling
-		UNITY_INSTANCING_CBUFFER_START(Props)
+		UNITY_INSTANCING_BUFFER_START(Props)
 			// put more per-instance properties here
-		UNITY_INSTANCING_CBUFFER_END
+		UNITY_INSTANCING_BUFFER_END(Props)
 
 		void surf (Input IN, inout SurfaceOutputStandard o) {
 			float heightPercent = inverseLerp(minHeight, maxHeight, IN.worldPos.y);
